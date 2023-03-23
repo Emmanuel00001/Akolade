3e7f7c8e01216fadb22d2e654d266d738b404eb1
Emmanuel00001 <39797773+Emmanuel00001@users.noreply.github.com>
2023-03-23 06:04:42 -0700

    Add files via upload

---
+++ My personal page.js
@@ -0,0 +1,11 @@
+// space, scary, military, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("cowboy")
+
+// watermelon, tomato, banana, orange, avocado, blueberry
+favouriteFruit("regular")
+
+// light, dark
+favouriteMode("light")
+
+// sharp, soft, round
+favouriteEdgeStyle("sharp")
